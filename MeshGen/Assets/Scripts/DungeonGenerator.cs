02f0d9391c3e8429a2086e5212fc7135373200de
J-galit <jaygalita@gmail.com>
2024-10-16 17:05:18 -0400

    Started the Mesh Generation Code

--- MeshGen/Assets/Scripts/DungeonGenerator.cs
+++ MeshGen/Assets/Scripts/DungeonGenerator.cs
@@ -31,12 +31,58 @@ public class DungeonGenerator : MonoBehaviour
 
     void MazeGenerator()
     {
+        board = new List<Cell>();
+
+        for (int i = 0; i < size.x; i++)
+        {
+            for( int j = 0; j < size.y; j++)
+            {
+                board.Add(new Cell());
+            }
+
+        }
+
+        int currentCell = startPos;
+
+        Stack<int> path = new Stack<int>();
+
+        int k = 0;
+
+        while(k < 1000)
+        {
+            k++;
+
+            board[currentCell].vistited = true;
+
+        }
+            
+               
 
     }
 
-    void CheckNeighbors()
+    List<int> CheckNeighbors(int cell)
     {
+        List<int> neighbors = new List<int>();
 
+        //check up neighbor
+        if (cell - size.x >= 0 && !board[Mathf.FloorToInt(cell - size.x)].vistited)
+        {
+            neighbors.Add(Mathf.FloorToInt(cell - size.x));
+        }
+
+        //check down neighbor
+        if (cell + size.x < board.Count && !board[Mathf.FloorToInt(cell + size.x)].vistited)
+        {
+            neighbors.Add(Mathf.FloorToInt(cell + size.x));
+        }
+
+        //check right neighbor
+        if ((cell + 1) % size.x < board.Count && !board[Mathf.FloorToInt(cell + size.x)].vistited)
+        {
+            neighbors.Add(Mathf.FloorToInt(cell + size.x));
+        }
+
+        return neighbors;
     }
 
 }
